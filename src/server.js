c2d003a663dabe83911b7ba6ded5ac02a75cf688
EduardaIF <ifeduarda.luiz@gmail.com>
2025-03-25 22:22:11 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express from "express";
 import UserRouter from "./routers/userRouter.js"
 import PropertyRouter from "./routers/propertyRouter.js"
+import cors from "cors"
 const app = express();
 const port = 3000;
-
 
 app.listen(port, ()=>{
     console.log(`Rodando em http://localhost:${port}`);
 })
+
+
 //MIddleWare
+app.use(cors()); // libera o cors para o navegador
+
 app.use(express.json());
 
 app.use("/user",UserRouter)
